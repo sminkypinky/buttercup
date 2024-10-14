abb25308217875ffe44643d3199473082f309b60
Adge <adgewalters@gmail.com>
2024-10-14 11:52:19 +0100

    Procfile rename

--- app.py
+++ app.py
@@ -217,4 +217,4 @@ def create_spotify_playlist():
         return jsonify({'error': f'Spotify API error: {str(e)}'}), 500
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
